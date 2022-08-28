a632eddee34b6215c27b7065d3f62dbcc75ceeaf
Nichakan Sukhumjitpitayotai <67417910+moondamon@users.noreply.github.com>
2022-08-28 14:05:52 +0700

    settings database

--- django project/aps_ce/aps_ce/settings.py
+++ django project/aps_ce/aps_ce/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'aps_ce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'moondamon',
+        'PASSWORD': 'moon10392',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
